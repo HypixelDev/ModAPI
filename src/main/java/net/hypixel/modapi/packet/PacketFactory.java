13bc42057b4955916799b43a05250ccac73319a3
Connor Linfoot <connorlinfoot@me.com>
2024-04-12 23:37:21 +0100

    Remove PacketFactory.java

--- src/main/java/net/hypixel/modapi/packet/PacketFactory.java
+++
@@ -1,8 +0,0 @@
-package net.hypixel.modapi.packet;
-
-import net.hypixel.modapi.serializer.PacketSerializer;
-
-public interface PacketFactory<P extends HypixelPacket> {
-
-	P create(PacketSerializer serializer);
-}
