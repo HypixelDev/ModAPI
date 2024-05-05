9600451a73847dcd9155c9c22503c6675440370a
Connor Linfoot <connorlinfoot@me.com>
2024-05-05 15:46:06 +0100

    update doc

--- src/main/java/net/hypixel/modapi/packet/EventPacket.java
+++ src/main/java/net/hypixel/modapi/packet/EventPacket.java
@@ -6,7 +6,7 @@ import net.hypixel.modapi.annotation.Experimental;
  * Represents a packet that is automatically sent based automatically based on specific conditions.
  * <p>
  * Event driven packets do not have a serverbound instance, as they can only be triggered by the server.
- * You will however, need to register that you wish to receive an event driven packet, see {@link net.hypixel.modapi.packet.impl.serverbound.ServerboundRegisterPacket}.
+ * You will however, need to subscribe to wanted events, see {@link net.hypixel.modapi.HypixelModAPI#subscribeToEventPacket(Class)}.
  */
 @Experimental
 public interface EventPacket extends ClientboundHypixelPacket {
